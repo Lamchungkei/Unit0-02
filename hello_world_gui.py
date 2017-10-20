1315d3d9042393d74c98bfc146ab277dab4a7b1f
Kay Lin <lamchuenkei@gmail.com>
2017-10-20 14:09:55 -0400

    Init

---
+++ hello_world_gui.py
@@ -0,0 +1,10 @@
+# Created by: Kay Lin
+# Created on: 12-Sep-2017
+# Created for: ICS3U
+# Daily Assignment - Unit0-02
+# This program displays Hello World but in GUI
+ 
+import ui
+
+v = ui.load_view()
+v.present('sheet')
